fe0578d6bc9ee2610cfd14ec22bcc6554a358358
Christopher Peterson <crpete23@gmail.com>
2018-07-10 14:02:29 -0700

    finished delete function

--- scripts/populate-posts.js
+++ scripts/populate-posts.js
@@ -12,7 +12,7 @@ function populatePostList(posts){
 
   list.innerHTML = lis.join(' ')
 
-  const eachLink = document.querySelectorAll('.list-group-item')
+  const eachLink = document.querySelectorAll('.list-group-item') //selects all of the post items in the side bar
   eachLink.forEach(function(ele) {
     const id= ele.getAttribute("name")
     const post = posts.find(obj => obj.id==id)
@@ -29,19 +29,31 @@ function populatePostList(posts){
                   <aside>
                     <ul class="nav">
                       <li class="nav-item">
-                        <a class="nav-link" id="edit-post">Edit</a>
+                        <a class="nav-link" id="edit-post" name="${post.id}">Edit</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link text-danger" id="delete-post">Delete</a>
+                        <a class="nav-link text-danger" id="delete-post" name="${post.id}">Delete</a>
                       </li>
                     </ul>
                   </aside>
                 </section>`;
     const content = document.querySelector('#content')
-
     content.innerHTML = update;
+
+    const deleteButton = document.querySelector('#delete-post')
+    const delID = deleteButton.getAttribute("name")
+    deleteButton.addEventListener("click", function(){
+      const delURL = `${URL}${delID}`
+      return axios.delete(delURL).then(function(result) {
+         console.log(result)
+         return renderPage()
+      })
+    })
     })
   })
+
+  const content = document.querySelector('#content')
+  content.innerHTML = ''; //clears the content portion after a new post has been made or site has been re-rendered
 }
 
 module.exports = {
